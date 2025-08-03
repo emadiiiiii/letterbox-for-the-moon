45479239d541542cb34a45d90272fcb937135ac1
e.ghosi <emadghosi.pw@gmail.com>
2025-08-03 17:54:06 +0330

    pwa

--- ervice-worker.js
+++ ervice-worker.js
@@ -1,12 +1,33 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installing...');
-  self.skipWaiting();
+const CACHE_NAME = "chick-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./jooje.html",
+  "./manifest.json",
+  "./media/chick_still.png",
+  "./media/chick_left.png",
+  "./media/chick_right.png",
+  "./media/heart.png",
+  "./media/broken_heart.png",
+  "./media/eat.wav",
+  "./media/heartbreak.wav",
+  "./media/jooje.mp3",
+  "./media/icon-192.png",
+  "./media/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activating...');
-});
-
-self.addEventListener('fetch', event => {
-  // You can add caching here if needed
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
